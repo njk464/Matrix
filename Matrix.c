8040f7259d327399c5267b13e1ffe298d89a3acb
njk464 <nick.kantor@utexas.edu>
2015-02-02 14:41:29 -0600

    Made the program read terminal size.

--- Matrix.c
+++ Matrix.c
@@ -5,26 +5,40 @@
 
 #include <stdlib.h>     /* srand, rand */
 #include <stdio.h>
+#include <sys/ioctl.h>
 
-char * randomeCharacterGenerator ();
-char * randomLineGenerator (int width)
+char randomeCharacterGenerator ();
+char randomCharacterGenerator ()
 {
-	char* line = "";
-	while (width > 0)
-	{
-		width--;
-		line += (char)(rand() % 67 + 67);
-	}
-	return line;
+	return (char)(rand() % 67 + 67);
 }
 
-int main () {
-	int n = 1;
-    while (n > 0)
-    {
-    	printf ("%s\n", randomLineGenerator (10));
-    }
-    return 0;
+int main (int argc, char* argv[]) {
+	// int n = 1;
+	// printf("HERE\n");
+	// struct winsize max;
+	// printf("HERE1\n");
+ //    ioctl(0, TIOCGWINSZ , &max);
+ //    printf("HERE2\n");
+ //    int columns = 0;
+ //    // printf ("lines %d\n", max.ws_row);
+ //    // printf ("columns %d\n", max.ws_col);
+ //    printf("HERE3\n");
+ //    //columns = max.ws_col;
+ //    while (n > 0)
+ //    {
+ //    	printf("HERE4\n");
+ //    	printf ("%s\n", randomLineGenerator (max.ws_col));
+ //    	printf("HERE5\n");
+ //    }
+ //    return 0;
+   struct winsize ws;
+   ioctl(0, TIOCGWINSZ, &ws);
+
+   int i=0;
+   for(;i<10*ws.ws_col;) printf("%c", randomCharacterGenerator());
+   printf("\n");
+   return 0;
 }
 
 
